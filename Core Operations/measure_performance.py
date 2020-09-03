6a042d714d5a45dff0ec17a58b5d7d937ca6a017
Hadley <Hadley_hzy@hotmail.com>
2020-09-03 19:45:11 +0200

    performance update

--- Core Operations/measure_performance.py
+++ Core Operations/measure_performance.py
@@ -13,4 +13,22 @@ t = (e2 - e1)/cv2.getTickFrequency()
 print(t)
 
 # Result I got is 0.521107655 seconds
+# pip install ipython 
+# type ipython to start ipython console
+
+# In [10]: x = 5
+
+# In [11]: %timeit y=x**2
+# 10000000 loops, best of 3: 73 ns per loop
+
+# In [12]: %timeit y=x*x
+# 10000000 loops, best of 3: 58.3 ns per loop
+
+# In [15]: z = np.uint8([5])
+
+# In [17]: %timeit y=z*z
+# 1000000 loops, best of 3: 1.25 us per loop
+
+# In [19]: %timeit y=np.square(z)
+# 1000000 loops, best of 3: 1.16 us per loop
 
